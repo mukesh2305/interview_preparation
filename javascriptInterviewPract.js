8338705bbdce535e437e1ce628a3e120200f48c8
jaydips0940 <yogahindu1999@gmail.com>
2022-04-25 08:33:09 +0530

    last commit =

--- javascriptInterviewPract.js
+++ javascriptInterviewPract.js
@@ -32,7 +32,7 @@ console.log("this")
 // asyncronuous  ----------------------------------------------
 console.log("I")
 console.log("am")
-setTimeout(function() {
+setTimeout(function () {
   console.log("eating")
 }, 1000);
 console.log("this")
@@ -40,7 +40,7 @@ console.log("this")
 // refere Book for javascript arcitucture ----------------------------------------
 
 // Hoisting realted starter
-var a ;
+var a;
 function value() {
   let num = 10;
   console.log("print this", num)
@@ -79,11 +79,11 @@ console.log("outside", a)
 // outside 15
 
 // callBack Hell => reoleve using promise ---------------------------------------------
-first(2, function(firstValue , err ) {
-  if(!err) {
-    two(firstValue, function(secondValue , err) {
-      if(!err) {
-        three(secondValue, function(thirdValue , err){
+first(2, function (firstValue, err) {
+  if (!err) {
+    two(firstValue, function (secondValue, err) {
+      if (!err) {
+        three(secondValue, function (thirdValue, err) {
           console.log(thirdValue)
         })
       }
@@ -95,24 +95,24 @@ first(2, function(firstValue , err ) {
 
 function first(value, callback) {
   callback(value + 1, false);
-} 
+}
 function two(value, callback) {
   callback(value + 1, false);
-} 
+}
 function three(value, callback) {
   callback(value + 1, false);
-} 
+}
 
 //  promise is used for resolving and simplifying code callback hell problem ---------------------------------------------------------
 function first(value) {
-  return value +1
-} 
+  return value + 1
+}
 function two(value) {
-  return value +1
-} 
+  return value + 1
+}
 function three(value) {
-  return value +1
-} 
+  return value + 1
+}
 
 let promise = new Promise((resolve, reject) => {
   resolve(2);
@@ -124,44 +124,44 @@ promise.then(first).then(two).then(three).then(res => console.log(res))
 
 // Promise.all ------------------------------------------------------------------
 let p1 = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('Success');
-        }, 2000);
-    });
-    
-   
+  setTimeout(() => {
+    resolve('Success');
+  }, 2000);
+});
 
-    let p2 = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('Success');
-        }, 2000);
-    });
 
-    let p3 = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('success');
-        }, 2000);
-    });
 
-    Promise.all([p1, p2, p3]).then(response => console.log(response));
+let p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('Success');
+  }, 2000);
+});
 
-  // async await ---------------------------------------------------------------------
-  async function value() {
+let p3 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('success');
+  }, 2000);
+});
+
+Promise.all([p1, p2, p3]).then(response => console.log(response));
+
+// async await ---------------------------------------------------------------------
+async function value() {
   console.log("I");
   await console.log("am");
   console.log("this");
 }
 value();
 
-(function() {
+(function () {
   console.log("after")
 })();
 
 // //  apply , call , bind method ---------------------------------------------------------------------
 
 let obj1 = {
-  name : "mukesh", 
-  print : function(surname)  {
+  name: "mukesh",
+  print: function (surname) {
     console.log(this.name + " " + surname);
   }
 }
@@ -170,11 +170,11 @@ obj1.print("purohit");
 
 
 let obj2 = {
-  name : "jaydeep"
+  name: "jaydeep"
 }
 
 obj1.print.call(obj2, "ram") // call
-obj1.print.apply(obj2,[ "ram"]) // apply
+obj1.print.apply(obj2, ["ram"]) // apply
 
 let just = obj1.print.bind(obj2) // bind 
 just("ram")
@@ -190,5 +190,5 @@ console.log(suppose)
 var suppose = value.reduce((acc, cur) => { //return single value 
   acc = acc + cur;
   return acc;
-}, 0) 
+}, 0)
 console.log(suppose)
